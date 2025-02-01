34cab62c51c9cf4216f8791b6b96c8677cff5951
0x7070 <raykhelson.ielon@gmail.com>
2025-02-01 18:36:31 -0500

    add and docstring tens_shapes func

--- info.py
+++ info.py
@@ -1,2 +1,23 @@
 ### HELPER FUNCTIONS
-# utils.info
+# model_utils/info
+
+def tens_shapes(*tensors: torch.Tensor) -> None:
+  """
+  Makes use of `torch.Tensor` if it is imported.
+
+  This function is based on the following operation:
+
+  ```
+  tensor.shape
+  ```
+
+  Args:
+    - `tensors`: Variable number of `torch.Tensor`s
+  Returns:
+    (ordered) torch.Size()
+  """
+  for idx, tensor in enumerate(tensors):
+    if isinstance(tensor, torch.Tensor):
+      print(f"Shape of tensor at arg{idx}: {tensor.shape}")
+    else:
+      print(f"Error: Cannot call `torch.Tensor.shape` on variable at arg{idx}.")
