4556d66cf6c71950be9e8f1671083f45057df88b
0x7070 <raykhelson.ielon@gmail.com>
2025-02-01 19:07:33 -0500

    add/doc tens_op

--- info.py
+++ info.py
@@ -24,3 +24,18 @@ def tens_shapes(*tensors: "torch.Tensor") -> None:
     else:
       print(f"Error: Cannot call `torch.Tensor.shape` on variable at arg{idx}.")
 
+def tens_op(*tensors: "torch.Tensor",
+            func=torch.Tensor.size) -> None:
+  """
+  For use with:
+  ```
+  tensors = (X_train, X_test, y_train, y_test)
+  tens_op(*tensors, torch.Tensor.size)
+  tens_op(X_train, X_test, y_train, y_test, func=len)
+  """
+  print(f"Operation: {func.__name__}")
+  for idx, tensor in enumerate(tensors):
+    if isinstance(tensor, torch.Tensor):
+      print(f"Result of operation on tensor at arg{idx}: {func(tensor)}")
+    else:
+      print(f"Error: Cannot call `{func.__name__}` on variable at arg{idx}.")
